85d760cd34ab7948361f1d2eecac7f6d59bf20fa
Yrii Kotyuk <yurasuao9@gmail.com>
2020-06-09 21:13:41 +0300

    added post items

---
+++ src/components/posts/DynamicPost.vue
@@ -0,0 +1,18 @@
+<template>
+	<div className="item-dynamic" v-if="post">
+		<h1>{{ post.type }} {{ post.timeout }}</h1>
+		{{ post }}
+	</div>
+</template>
+
+<script lang="ts">
+	import {Component, Vue, Prop} from 'vue-property-decorator'
+	import Post from '@/types/posts/Post'
+
+	@Component({
+		components: {},
+	})
+	export default class DynamicPost extends Vue {
+		@Prop({ required: true }) post: Post
+	}
+</script>
---
+++ src/components/posts/StaticPost.vue
@@ -0,0 +1,26 @@
+<template>
+	<div className="item-static" v-if="post">
+		<h1>{{ post.type }}</h1>
+		{{ post }}
+	</div>
+</template>
+
+<script lang="ts">
+	import {Component, Vue} from 'vue-property-decorator'
+	import Post from '@/types/posts/Post'
+
+	@Component({
+		components: {},
+	})
+	export default class StaticPost extends Vue {
+		get post(): Post {
+			return this.$accessor.posts.getPostBySlug(this.$route.params.slug)
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.item {
+		padding: 100px;
+	}
+</style>
